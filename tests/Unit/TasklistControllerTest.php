ce57c8ef1dece0cab8529ce5180d97f1ec622770
Joao Brandao <joao.brandao@capitalise.com>
2020-05-09 18:26:59 +0100

    wip
    wip

--- tests/Unit/TasklistControllerTest.php
+++ tests/Unit/TasklistControllerTest.php
@@ -10,18 +10,15 @@ use Mockery as m;
 
 class TasklistControllerTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @test
-     */
+    /** @test */
     public function index_when_empty_collection()
     {
-
         $mock = m::mock(Tasklist::class)
-            ->shouldReceive([
-                'all' => new Collection()
-            ])
+            ->shouldReceive(
+                [
+                    'all' => new Collection()
+                ]
+            )
             ->getMock();
 
 
@@ -31,29 +28,70 @@ class TasklistControllerTest extends TestCase
         $this->assertTrue($tasklists->isEmpty());
     }
 
-    
-    /**
-     * A basic unit test example.
-     *
-     * @test
-     */
+    /** @test */
     public function index_when_not_empty_collection()
     {
-    
         $collection = new Collection();
         $tasklist = new Tasklist();
         $collection->push($tasklist);
 
         $mock = m::mock(Tasklist::class)
-            ->shouldReceive([
-                'all' => $collection
-            ])
+            ->shouldReceive(
+                [
+                    'all' => $collection
+                ]
+            )
             ->getMock();
 
         $controller = new TasklistController($mock);
+        /** @var Collection $tasklists */
         $tasklists = $controller->index();
         $this->assertInstanceOf(Collection::class, $tasklists);
         $this->assertTrue($tasklists->count() > 0);
         $this->assertInstanceOf(Tasklist::class, $tasklists->first());
+    }
+
+    /**
+     * @test
+     * @dataProvider indexDataProvider
+     * @param Collection $all
+     * @param bool $isEmpty
+     */
+    public function index(Collection $all, bool $isEmpty)
+    {
+        $mock = m::mock(Tasklist::class)
+            ->shouldReceive(
+                [
+                    'all' => $all
+                ]
+            )
+            ->getMock();
+
+        $controller = new TasklistController($mock);
+        /** @var Collection $tasklists */
+        $tasklists = $controller->index();
+
+        $this->assertInstanceOf(Collection::class, $tasklists);
+        $this->assertEquals($isEmpty, $tasklists->isEmpty());
+    }
+
+    public function indexDataProvider()
+    {
+        return [
+            'When all returns an empty collection' => [
+                'all' => new Collection(),
+                'isEmpty' => true
+            ],
+            'When all returns a collection with data' => [
+                'all' => (new Collection())->push(new Tasklist()),
+                'isEmpty' => false
+            ],
+        ];
+    }
+
+    protected function tearDown(): void
+    {
+        // this will close mocks before each test.
+        m::close();
     }
 }
